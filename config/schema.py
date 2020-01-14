dbec92ecc66a5a5af58bfd606f288b97e2434d11
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2020-01-14 19:22:27 +0900

    #4.5 Queries Using Graphene Django

--- config/schema.py
+++ config/schema.py
@@ -1,11 +1,22 @@
 import graphene
+from graphene_django import DjangoObjectType
+from rooms.models import Room
+
+
+class RoomType(DjangoObjectType):
+    class Meta:
+        model = Room
 
 
 class Query(graphene.ObjectType):
     hello = graphene.String()
+    rooms = graphene.List(RoomType)
 
     def resolve_hello(self, info):
         return "Hello"
+
+    def resolve_rooms(self, info):
+        return Room.objects.all()
 
 
 class Mutation:
