ed98d9fc6c9bac5b291bb69d6f74deb4d482710a
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2020-01-15 19:54:51 +0900

    #5.9 Dynamic Fields

--- rooms/types.py
+++ rooms/types.py
@@ -4,8 +4,17 @@ from .models import Room
 
 
 class RoomType(DjangoObjectType):
+
+    is_fav = graphene.Boolean()
+
     class Meta:
         model = Room
+
+    def resolve_is_fav(room, info):
+        user = info.context.user
+        if user.is_authenticated:
+            return room in user.favs.all()
+        return False
 
 
 class RoomListResponse(graphene.ObjectType):
